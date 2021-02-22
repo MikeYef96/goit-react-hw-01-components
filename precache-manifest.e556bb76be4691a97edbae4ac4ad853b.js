b3a5b8354282c7d8a2d9e36afd7340101157dec4
MikeYef96 <mike.yef@gmail.com>
2021-02-22 21:04:40 +0300

    Updates

---
+++ precache-manifest.e556bb76be4691a97edbae4ac4ad853b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d057aa2adf703c2e1bc631b30daabac7",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "ca4dcb91fac12be75886",
+    "url": "/goit-react-hw-01-components/static/css/main.090480dd.chunk.css"
+  },
+  {
+    "revision": "3a94690c979982ff4c7c",
+    "url": "/goit-react-hw-01-components/static/js/2.b5aa2a49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.b5aa2a49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca4dcb91fac12be75886",
+    "url": "/goit-react-hw-01-components/static/js/main.c9b0d5f9.chunk.js"
+  },
+  {
+    "revision": "3bd89fc3734ea58c1e71",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.be0b3354.js"
+  }
+]);
